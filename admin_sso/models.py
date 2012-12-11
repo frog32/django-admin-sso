1f8284f6787bbd19d10cb7709e88fbeaf6778187
Marc Egli <frog32@me.com>
2012-12-11 10:02:59 +0100

    stay compatible with django < 1.5

--- admin_sso/models.py
+++ admin_sso/models.py
@@ -8,10 +8,7 @@ except ImportError:
 
 from admin_sso import settings
 
-try:
-    User = settings.AUTH_PROFILE_MODULE
-except AttributeError:
-    from django.contrib.auth.models import User
+User = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
 class Assignment(models.Model):
@@ -20,7 +17,7 @@ class Assignment(models.Model):
     domain = models.CharField(max_length=255)
     copy = models.BooleanField(default=False)
     weight = models.PositiveIntegerField(default=0)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+    user = models.ForeignKey(User, null=True, blank=True)
 
     class Meta:
         verbose_name = _('Assignment')
@@ -35,7 +32,7 @@ class OpenIDUser(models.Model):
     claimed_id = models.TextField(max_length=2047)
     email = models.EmailField()
     fullname = models.CharField(max_length=255)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user = models.ForeignKey(User)
     last_login = models.DateTimeField(_('last login'), default=now)
 
     class Meta:
